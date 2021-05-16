0894cf27200e5254fd79c0333331acddaf27e13f
Puscuta Alexandru-Ionut <74986114+lefion10@users.noreply.github.com>
2021-05-16 15:21:25 +0300

    Event class
    Event  constructor and other methods

--- src/com/company/Event.java
+++ src/com/company/Event.java
@@ -1,26 +1,89 @@
 package com.company;
+import java.util.Scanner;
 
 public class Event {
     private String eventName ;
     private String eventLocation ;
     private String eventDate;
     private int eventPrize;
-    private String eventDescription;
-    private int eventBudget;
-    private int organizingCost = 1000;
+    private int eventCost;
+    private static int organizingCost = 0;
+//    Event ID
+    private static int id = 0;
+    private int eventSport ;
+    private int eventId = 0;
+
+//    Event constructor
 
     public Event(){
+        id++;
         Scanner snc = new Scanner(System.in);
+
         System.out.println("Event name: ");
         this.eventName = snc.nextLine();
+
         System.out.println("Event location: ");
         this.eventLocation = snc.nextLine();
+
         System.out.println("Event Date: ");
         this.eventDate = snc.nextLine();
+
+        System.out.println("1 - Chess \n2 - Tennis \n3 Basketball \n4 Soccer : ");
+        this.eventSport = snc.nextInt();
+
         System.out.println("Event Prize: ");
         this.eventPrize = snc.nextInt();
-        System.out.println("Event Description: ");
-        this.eventDescription = snc.nextLine();
-        System.out.println(eventDescription);
+
+        setEventId();
+        setOrganizingCost();
+        getInfo();
+
+
     }
+
+    private void setEventId(){
+        id++;
+        this.eventId = eventSport + 1000 + id;
+        System.out.println("The Id of the event is: " + eventId );
+    }
+
+
+    public void setOrganizingCost(){
+        Scanner snc = new Scanner(System.in);
+
+        System.out.println("Number of players: ");
+        int playersNumb = snc.nextInt();
+
+        System.out.println("Number of seats: ");
+        int seatsNumb = snc.nextInt();
+
+        System.out.println("Duration of the event: ");
+        int eventDuration = snc.nextInt();
+
+        this.eventCost = playersNumb * 100 + seatsNumb * 10 + eventDuration * 50 + eventPrize;
+        System.out.println("The cost of the event is : " + eventCost + "$" );
+
+    }
+
+    public String getInfo() {
+        switch (eventSport) {
+            case 1:
+                return eventName + " is an Chess event , starting at " + eventDate + " at " + eventLocation;
+
+            case 2:
+                return eventName + " is an Tennis event , starting at " + eventDate + " at " + eventLocation;
+
+            case 3:
+                return eventName + " is an Basketball event , starting at " + eventDate + " at " + eventLocation;
+
+            case 4:
+                return eventName + " is an Soccer event , starting at " + eventDate + " at " + eventLocation;
+            default:
+                return "There s no information about this event";
+        }
+    }
+
+
 }
+
+
