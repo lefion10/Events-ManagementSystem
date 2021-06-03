8ee4d431161d87e09e42f6c4ef5f0c33b4fd0da6
Puscuta Alexandru-Ionut <74986114+lefion10@users.noreply.github.com>
2021-06-03 15:50:16 +0300

    Update Event.java

--- src/com/company/Event.java
+++ src/com/company/Event.java
@@ -2,122 +2,77 @@ package com.company;
 import java.util.*;
 
 public class Event {
-    private String eventName ;
-    private String eventLocation ;
-    private String eventDate;
-    private int eventPrize;
-    private int eventCost;
-    private static int organizingCost = 0;
-//    Event ID
-    private static int id = 1000;
-    private int eventSport ;
-    private int eventId = 0;
+    private final String _eventName;
+    private final Date _eventDate;
+    private final int _eventPrize;
+    private final int _id;
+    private final int _sportId;
+    private final int _locationId;
 
-//    Event constructor
-
-    public Event(){
-        id++;
-        Scanner snc = new Scanner(System.in);
-
-        System.out.println("Event name: ");
-        this.eventName = snc.nextLine();
-
-        System.out.println("Event location: ");
-        this.eventLocation = snc.nextLine();
-
-        System.out.println("Event Date: ");
-        this.eventDate = snc.nextLine();
-
-        System.out.println("1 - Chess \n2 - Tennis \n3 Basketball \n4 Soccer : ");
-        try{
-            do{
-                this.eventSport = snc.nextInt();
-                if(eventSport > 0 && eventSport < 5){
-                    break;
-                }
-                else{
-                    continue;
-                }
-            }
-            while(true);
-        }
-        catch (Throwable throwable){
-            System.out.println("You did not choose one of the options");
-        }
-
-        System.out.println("Event Prize: ");
-        try{
-            this.eventPrize = snc.nextInt();
-        }
-        catch (Throwable throwable){
-            System.out.println("You did not choose a valid amount");
-        }
-
-        setEventId();
-        setOrganizingCost();
-        getInfo();
-
-
+    public Event(String eventName, int locationId, Date eventDate, int eventPrize, int id, int sportId ){
+        _eventName = eventName;
+        _locationId = locationId;
+        _eventDate = eventDate;
+        _eventPrize = eventPrize;
+        _id = id;
+        _sportId =sportId;
     }
 
-    private void setEventId(){
-        this.eventId = eventSport + id;
-        System.out.println("The Id of the event is: " + eventId );
+    public String getName(){
+        return _eventName;
     }
 
-
-    public void setOrganizingCost(){
-        Scanner snc = new Scanner(System.in);
-
-        System.out.println("Number of players: ");
-        int playersNumb = snc.nextInt();
-
-        System.out.println("Number of seats: ");
-        int seatsNumb = snc.nextInt();
-
-        System.out.println("Duration of the event: ");
-        int eventDuration = snc.nextInt();
-
-        this.eventCost = playersNumb * 100 + seatsNumb * 10 + eventDuration * 50 + eventPrize;
-        System.out.println("The cost of the event is : " + eventCost + "$" );
-
+    public Date getDate(){
+        return _eventDate;
     }
 
-    public void getInfo() {
-        switch (eventSport) {
-            case 0:
-                System.out.println(eventName + " is an Sport event , starting at " + eventDate + " at " + eventLocation);
-            case 1:
-                System.out.println(eventName + " is an Chess event , starting at " + eventDate + " at " + eventLocation);
-            case 2:
-                System.out.println(eventName + " is an Tennis event , starting at " + eventDate + " at " + eventLocation);
-            case 3:
-                System.out.println(eventName + " is an Basketball event , starting at " + eventDate + " at " + eventLocation);
-            case 4:
-                System.out.println(eventName + " is an Soccer event , starting at " + eventDate + " at " + eventLocation);
-            default:
-                System.out.println("There s no information about this event");
-        }
+    public int getLocation(){
+        return _locationId;
+    }
+
+    public int getPrize(){
+        return _eventPrize;
     }
 
     public int getId(){
-        return id;
+        return _id;
+    }
+    public int getSportId(){
+        return _sportId;
     }
 
+    @Override
     public int hashCode(){
-        return id;
+        final int prime = 5;
+        int hash = 1;
+        hash = prime * prime + _id;
+        hash = prime * prime + _sportId;
+        hash = prime * prime + _locationId;
+        return hash;
     }
 
+    @Override
     public boolean equals(Object obj) {
         if (obj == null) return false;
         if (!(obj instanceof Event))
             return false;
         if (obj == this)
             return true;
-        return this.getId() == ((Event) obj).getId();
+        Event other = (Event) obj;
+        if (_id != other._id)
+            return false;
+        if (_locationId != other._locationId)
+            return false;
+        if (_sportId != other._sportId)
+            return false;
+        if (_eventDate == null){
+            if(other._eventDate != null){
+                return false;
+            }
+        }else if (!_eventDate.equals(other._eventDate))
+            return false;
+        return true;
     }
-
-
 }
 
 
